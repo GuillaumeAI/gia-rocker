46bea5153704dd1fe7afbaea5b796ea38c3b509b
JGWill Gaia.WSL <jgi@jgwill.com>
2022-03-22 00:54:18 -0400

    add:Run R in Jupyther (Works)

---
+++ x/x__R_in_Gupyther__220322/_env.sh
@@ -0,0 +1,8 @@
+export dockertag=guillaumeai/server:gupyter-tf-ds2
+export dockertag=jupyter/datascience-notebook
+export containername=gupy-220322
+export containername=gupy-220322-datascience-notebook
+
+export dkextra="--rm -p 8888:8888"
+#export dkextra="--rm -p 8888:8888"
+export custompwdmountpath=/home/jovyan/work
